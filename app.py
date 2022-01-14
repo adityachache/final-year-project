a3fa9e3b53b2d174ff02fdb53ff55e10bfd24bb3
adityachache <aditya.chache@gmail.com>
2022-01-14 14:29:57 +0530

    'second'

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from multiapp import Multiapp
 from pages import home, filtering, compression, realtime
-try:
-    import streamlit.ReportThread as ReportThread
-    from streamlit.server.Server import Server
-except Exception:
-    # Streamlit >= 0.65.0
-    import streamlit.report_thread as ReportThread
-    from streamlit.server.server import Server
+# try:
+#     import streamlit.ReportThread as ReportThread
+#     from streamlit.server.Server import Server
+# except Exception:
+#     # Streamlit >= 0.65.0
+#     import streamlit.report_thread as ReportThread
+#     from streamlit.server.server import Server
 
 app = Multiapp()
 
