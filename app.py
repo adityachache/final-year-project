258f2585c001233868d4d126c76b25e02d288e3e
adityachache <aditya.chache@gmail.com>
2022-01-14 16:41:04 +0530

    fourth

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-import streamlit as st
 from multiapp import Multiapp
 from pages import home, filtering, compression, realtime
+import streamlit as st
+from streamlit.hashing import _CodeHasher
 
+try:
+    # Before Streamlit 0.65
+    from streamlit.ReportThread import get_report_ctx
+    from streamlit.server.Server import Server
+except ModuleNotFoundError:
+    # After Streamlit 0.65
+    from streamlit.report_thread import get_report_ctx
+
+    
 app = Multiapp()
 
 app.add_app("Home", home.app)
